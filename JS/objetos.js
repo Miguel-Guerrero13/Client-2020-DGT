d1013fa6efd25e5fa78106b19ded1b068fe31732
Miguel-Guerrero13 <73499244+Miguel-Guerrero13@users.noreply.github.com>
2020-11-26 18:56:41 +0100

    Update objetos.js

---
+++ JS/objetos.js
@@ -0,0 +1,31 @@
+//Creación del objeto persona en método nuevo con el toHtmlRow
+
+class Persona {
+	constructor(sNif,sNombre,sApellidos,sDireccion){
+        this.dni=sNif;
+        this.nombre=sNombre;
+        this.apellidos=sApellidos;
+        this.direccion=sDireccion;
+    }
+}
+
+Persona.prototype.toHTMLRow = function (){
+
+    let sFila = "<tr>";
+    sFila+="<td>"+this.dni+"</td>";
+    sFila+="<td>"+this.nombre+"</td>";
+    sFila+="<td>"+this.apellidos+"</td>";
+    sFila+="<td>"+this.direccion+"</td>";
+    sFila += "</tr>";
+    
+    return sFila;
+    }
+
+    class Conductor extends Persona{
+
+        constructor(dCaducidadCarnet){
+
+            this.caducidadCarnet=dCaducidadCarnet;
+            
+        }
+    }
