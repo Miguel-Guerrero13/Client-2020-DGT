1adf36c5f6d309daff4cf00d84ccad7ef48b9e00
Miguel-Guerrero13 <73499244+Miguel-Guerrero13@users.noreply.github.com>
2020-11-30 09:05:21 +0100

    Add files via upload

---
+++ Client-2020-DGT-main/JS/codigo.js
@@ -0,0 +1,116 @@
+var oDGT = new DGT();
+
+ocultarFormularios();
+
+function ocultarFormularios(){
+frmAltaConductor.style.display = "none";
+frmAltaGuardia.style.display = "none";
+frmRegistrarMulta.style.display="none";
+frmPagaMulta.style.display="none";
+}
+
+function mostrarAltaConductor(){
+ocultarFormularios();
+frmAltaConductor.style.display = "block";
+}
+
+function mostrarPagaMulta(){
+    ocultarFormularios();
+    frmPagaMulta.style.display = "block";
+    }
+
+function mostrarAltaGuardia(){
+ocultarFormularios();
+frmAltaGuardia.style.display = "block";
+}
+function mostrarRegistrarMulta()
+{
+    ocultarFormularios();
+    frmRegistrarMulta.style.display="block";
+}
+
+function aceptarAltaConductor(){
+
+    let sNif = frmAltaConductor.txtCodigo.value.trim();
+    let sNombre = frmAltaConductor.txtNombre.value.trim();
+    let sApellidos = frmAltaConductor.txtCodigo.value.trim();
+    let sDireccion = frmAltaConductor.txtNombre.value.trim();
+    let dCaducidadCarnet= frmAltaConductor.txtFecha.value.trim();
+
+    let oC = new Conductor(sNif,sNombre, sApellidos, sDireccion, dCaducidadCarnet);
+
+    // Inserto en el almacen
+    oDGT.altaConductor(oC);
+
+    ocultarFormularios();
+}
+
+function aceptarPagaMulta(){
+
+    let idMulta = frmPagaMulta.txtMulta.value.trim();
+
+    let oC = new Multa(idMulta);
+
+    // Inserto en el almacen
+    oDGT.pagaMulta(oC);
+
+    ocultarFormularios();
+}
+
+function aceptarAltaGuardia(){
+
+    let sNif = frmAltaGuardia.txtCodigo.value.trim();
+    let sNombre = frmAltaGuardia.txtNombre.value.trim();
+    let sApellidos = frmAltaGuardia.txtPrecio.value.trim();
+    let sDireccion = frmAltaGuardia.txtUnidades.value.trim();
+    let dpuesto= frmAltaGuardia.txtPuesto.value.trim();
+
+    let oC = new GuardiaCivil(sNif,sNombre, sApellidos, sDireccion, dpuesto);
+
+    // Inserto en el almacen
+    oDGT.altaGuardia(oC);
+
+    ocultarFormularios();
+}
+
+function aceptarRegistrarMulta(){
+
+    let iMulta = parseInt(frmRegistrarMulta.txtIdMulta.value.trim());
+    let sNifConductor = frmRegistrarMulta.txtNifConductor.value.trim();
+    let sNifGuardia = frmRegistrarMulta.txtNifGuardia.value.trim();
+    let fImporteMulta = parseFloat(frmRegistrarMulta.txtImporteMulta.value.trim());
+    let bPagada = false;
+    let sDescripcionMulta= frmRegistrarMulta.txtDescripcionMulta.value.trim();
+    let dFechaMulta= frmRegistrarMulta.txtFechaMulta.value.trim();
+    let bTipoMulta = frmRegistrarMulta.inlineCheckbox1.value.trim();
+    let bBonificada = frmRegistrarMulta.inlineRadio1.value.trim();
+    let iPuntosMulta = parseInt(frmRegistrarMulta.puntos.value.trim());
+    if(bTipoMulta == "Leve")
+    {
+        let oM = new Leve(iMulta,sNifConductor, sNifGuardia, fImporteMulta,bPagada ,sDescripcionMulta,dFechaMulta,bBonificada);
+        oDGT.altaMultaLeve(oM);
+    }
+    else
+    {
+        if(iPuntosMulta>=1 && iPuntosMulta<=15){
+
+            let oM = new Grave(iMulta,sNifConductor, sNifGuardia, fImporteMulta,bPagada ,sDescripcionMulta,dFechaMulta,iPuntosMulta);
+        oDGT.altaMultaGrave(oM);
+
+        }else{
+
+            alert("Introduce unos puntos entre 1 y 15");
+
+        }
+        
+    }
+
+    
+
+    // Inserto en el almacen
+  
+
+    ocultarFormularios();
+}
+
+
---
+++ Client-2020-DGT-main/JS/objetos.js
@@ -0,0 +1,274 @@
+//Creación del objeto persona en método antiguo con el toHtmlRow
+
+function Persona(sNif,sNombre,sApellidos,sDireccion){
+    this.dni=sNif;
+        this.nombre=sNombre;
+        this.apellidos=sApellidos;
+        this.direccion=sDireccion;
+ }
+
+ Persona.prototype.toHTMLRow = function (){
+
+    let sFila = "<tr>";
+    sFila+="<td>"+this.dni+"</td>";
+    sFila+="<td>"+this.nombre+"</td>";
+    sFila+="<td>"+this.apellidos+"</td>";
+    sFila+="<td>"+this.direccion+"</td>";
+    sFila += "</tr>";
+    
+    return sFila;
+    }
+
+    function Conductor(sNif,sNombre,sApellidos,sDireccion,dCaducidadCarnet)
+    {
+        Persona.call(this,sNif,sNombre,sApellidos,sDireccion);
+        this.caducidadCarnet=dCaducidadCarnet;
+    }
+    
+    // Aqui es donde heredamos propiedades y metodos
+    Conductor.prototype = Object.create(Persona.prototype);
+    Conductor.prototype.constructor = Conductor;
+
+    Conductor.prototype.toHTMLRow = function (){
+
+        let sFila = "<tr>";
+        sFila+="<td>"+this.dni+"</td>";
+        sFila+="<td>"+this.nombre+"</td>";
+        sFila+="<td>"+this.apellidos+"</td>";
+        sFila+="<td>"+this.direccion+"</td>";
+        sFila+="<td>"+this.caducidadCarnet+"</td>";
+        sFila += "</tr>";
+        
+        return sFila;
+        }
+
+
+        function GuardiaCivil(sNif,sNombre,sApellidos,sDireccion,sPuesto)
+        {
+            Persona.call(this,sNif,sNombre,sApellidos,sDireccion);
+            this.puesto=sPuesto;
+        }
+        
+        // Aqui es donde heredamos propiedades y metodos
+        GuardiaCivil.prototype = Object.create(Persona.prototype);
+        GuardiaCivil.prototype.constructor = GuardiaCivil;
+
+    
+        Conductor.prototype.toHTMLRow = function (){
+    
+            let sFila = "<tr>";
+            sFila+="<td>"+this.dni+"</td>";
+            sFila+="<td>"+this.nombre+"</td>";
+            sFila+="<td>"+this.apellidos+"</td>";
+            sFila+="<td>"+this.direccion+"</td>";
+            sFila+="<td>"+this.puesto+"</td>";
+            sFila += "</tr>";
+            
+            return sFila;
+            }
+
+
+//Clase nueva
+
+class Multa {
+	constructor(iIdMulta,sNifConductor,sNifGuardia, fImporte,bPagada,sDescripcion,dFecha){
+        this.idMulta=iIdMulta;
+        this.nifConductror=sNifConductor;
+        this.nifGuardia=sNifGuardia;
+        this.importe=fImporte;
+        this.pagada=bPagada;
+        this.descripcion=sDescripcion;
+        this.fecha=dFecha;
+    }
+}
+
+Multa.prototype.toHTMLRow = function (){
+
+    let sFila = "<tr>";
+    sFila+="<td>"+this.idMulta+"</td>";
+    sFila+="<td>"+this.nifConductror+"</td>";
+    sFila+="<td>"+this.nifGuardia+"</td>";
+    sFila+="<td>"+this.importe+"</td>";
+    sFila+="<td>"+this.pagada+"</td>";
+    sFila+="<td>"+this.descripcion+"</td>";
+    sFila+="<td>"+this.fecha+"</td>";
+    sFila += "</tr>";
+    
+    return sFila;
+    }
+
+    
+    class Leve extends Multa{
+
+        constructor(iIdMulta,sNifConductor,sNifGuardia, fImporte,bPagada,sDescripcion,dFecha,bBonificada){
+            super(iIdMulta,sNifConductor,sNifGuardia, fImporte,bPagada,sDescripcion,dFecha);
+            this.bonificada=bBonificada;
+
+        }
+    }
+
+    Leve.prototype.toHTMLRow = function (){
+
+        let sFila = "<tr>";
+        sFila+="<td>"+this.idMulta+"</td>";
+        sFila+="<td>"+this.nifConductror+"</td>";
+        sFila+="<td>"+this.nifGuardia+"</td>";
+        sFila+="<td>"+this.importe+"</td>";
+        sFila+="<td>"+this.pagada+"</td>";
+        sFila+="<td>"+this.descripcion+"</td>";
+        sFila+="<td>"+this.fecha+"</td>";
+        sFila+="<td>"+this.bonificada+"</td>";
+        sFila += "</tr>";
+        
+        return sFila;
+        }
+
+
+        class Grave extends Multa{
+
+            constructor(iIdMulta,sNifConductor,sNifGuardia, fImporte,bPagada,sDescripcion,dFecha,iPuntos){
+                super(iIdMulta,sNifConductor,sNifGuardia, fImporte,bPagada,sDescripcion,dFecha);
+                this.puntos=iPuntos;
+    
+            }
+        }
+
+        Grave.prototype.toHTMLRow = function (){
+
+            let sFila = "<tr>";
+            sFila+="<td>"+this.idMulta+"</td>";
+            sFila+="<td>"+this.nifConductror+"</td>";
+            sFila+="<td>"+this.nifGuardia+"</td>";
+            sFila+="<td>"+this.importe+"</td>";
+            sFila+="<td>"+this.pagada+"</td>";
+            sFila+="<td>"+this.descripcion+"</td>";
+            sFila+="<td>"+this.fecha+"</td>";
+            sFila+="<td>"+this.puntos+"</td>";
+            sFila += "</tr>";
+            
+            return sFila;
+            }
+
+
+
+
+class DGT {
+	constructor(){
+        this.multas=[];
+        this.personas=[];
+    }
+
+    altaConductor(oC){
+
+        let oConductorExistente = null;
+
+        oConductorExistente = this._buscarPersona(oC.dni);
+
+        if(oConductorExistente==null)
+        {
+           this.personas.push(oC);
+           alert("Alta de conductor realizada");
+        }
+           else
+           alert("Ya hay una persona con ese dni");
+        
+        }
+
+        altaGuardia(oC){
+
+            let oGuardiaExistente = null;
+    
+            oGuardiaExistente = this._buscarPersona(oC.dni);
+    
+            if(oGuardiaExistente==null)
+            {
+               this.personas.push(oC);
+               alert("Alta de conductor realizada");
+            }
+               else
+               alert("Ya hay una persona con ese dni");
+            
+            }
+
+            altaMultaLeve(oM){
+
+
+                let oMultaExistente = null;
+    
+                oMultaExistente = this._buscarMultas(oM.idMulta);
+    
+            if(oMultaExistente==null)
+            {
+                this.multas.push(oM);
+               alert("Alta de multa realizada");
+            }
+               else
+               alert("Ya hay una multa con ese id");
+
+                }
+
+            
+            
+            altaMultaGrave(oM){
+        
+                let oMultaExistente = null;
+    
+                oMultaExistente = this._buscarMultas(oM.idMulta);
+    
+            if(oMultaExistente==null)
+            {
+                this.multas.push(oM);
+               alert("Alta de multa realizada");
+            }
+               else
+               alert("Ya hay una multa con ese id");
+                 
+                 }
+
+
+                 pagaMulta(oM){
+        
+                    let oMultaExistente = null;
+        
+                    oMultaExistente = this._buscarMultas(oM.idMulta);
+        
+                if(oMultaExistente==null)
+                {
+                   alert("No existe ninguna multa con ese nombre");
+                }else if(oMultaExistente.pagada==false){
+                    
+                    oMultaExistente.pagada=true;
+                    alert("Multa pagada");
+                    
+                }else{
+
+                   alert("Multa ya pagada anteriormente");
+                }
+                     }
+            
+
+
+
+
+    _buscarPersona(sDni){
+
+      let oConductorExistente=null;
+
+        oConductorExistente=this.personas.find(oC => oC.dni==sDni);
+
+        return oConductorExistente;
+
+    }
+
+    _buscarMultas(sIdMulta){
+
+        let oMultaExistente=null;
+  
+          oMultaExistente=this.multas.find(oM => oM.idMulta==sIdMulta);
+  
+          return  oMultaExistente;
+  
+      }
+
+    
+}
